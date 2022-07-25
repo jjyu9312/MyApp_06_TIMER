16dbfbadfbe8574697153a0da19ea15c3b513587
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-07-26 01:04:32 +0900

    07/26 commit

--- app/src/main/java/com/kkuber/myapp_06_timer/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_06_timer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kkuber.myapp_06_timer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.SeekBar
 import android.widget.TextView
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private var currentCountDownTimer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,11 +41,52 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onStartTrackingTouch(p0: SeekBar?) {
+
+                    currentCountDownTimer?.cancel() // 현재 카운트다운 타이머 멈춤
+                    currentCountDownTimer = null
+
                 }
 
                 override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                    seekBar ?: return
+
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
                 }
             }
         )
+    }
+
+    private fun createCountDownTimer(initialMillis: Long) =
+
+        object: CountDownTimer(initialMillis, 1000L) {
+            override fun onTick(p0: Long) {
+
+                updateRemainingTimes(p0) // 1초 마다 한번씩 텍스트 뷰 갱신
+                updateSeekBar(p0) // 1초 마다 Seek Bar 갱신
+
+            }
+
+            override fun onFinish() {
+
+                updateRemainingTimes(0)
+                updateSeekBar(0)
+
+            }
+        }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateRemainingTimes(remainMillis: Long) {
+
+        val remainSeconds = remainMillis / 1000
+
+        remianMinutesTextView.text = "%02d".format(remainSeconds / 60)
+        remainSecondsTextView.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+
+        seekBar.progress = (remainMillis / 1000 / 60).toInt()
     }
 }
